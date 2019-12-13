beec19a2a74d84de89e34dca770fa2c230eec02b
Alex Hemsath <ahemsath@newrelic.com>
2019-12-12 16:02:45 -0800

    Add unit tests project for NLog with a single initial test

---
+++ src/NLog/NewRelic.LogEnrichers.NLog.Tests/LayoutTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using NLog.Targets;
+using NLog.Config;
+using NLog;
+using System.Threading;
+using System.Diagnostics;
+using NLog.LayoutRenderers;
+
+namespace NewRelic.LogEnrichers.NLog.Tests
+{
+    public class LayoutTests
+    {
+        Logger _logger;
+        DebugTarget _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new DebugTarget("testTarget");
+            _target.Layout = new NewRelicJsonLayout();
+
+            var config = new LoggingConfiguration();
+            config.AddTarget(_target);
+            config.AddRuleForAllLevels(_target);
+
+            LogManager.Configuration = config;
+
+            _logger = LogManager.GetLogger("testLogger");
+        }
+
+        [Test]
+        public void LogMessage_VerifyAttributes()
+        {
+            var message = "lorem ipsum";
+            _logger.Info(message);
+
+            var loggedMessage = _target.LastMessage;
+            var resultsDictionary = TestHelpers.DeserializeOutputJSON(loggedMessage);
+
+            Asserts.KeyAndValueMatch(resultsDictionary, "message", message);
+            Asserts.KeyAndValueMatch(resultsDictionary, "log.level", "INFO");
+            Asserts.KeyAndValueMatch(resultsDictionary, "thread.id", Thread.CurrentThread.ManagedThreadId.ToString());
+            Asserts.KeyAndValueMatch(resultsDictionary, "process.id", Process.GetCurrentProcess().Id.ToString());
+            Assert.IsTrue(resultsDictionary.ContainsKey("timestamp"));
+        }
+    }
+}
---
+++ src/NLog/NewRelic.LogEnrichers.NLog.Tests/TestHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace NewRelic.LogEnrichers.NLog.Tests
+{
+    public static class TestHelpers
+    {
+        /// <summary>
+        /// Responsible for deserializing the JSON that was built.
+        /// Will ensure that the JSON is well-formed
+        /// </summary>
+        public static  Dictionary<string, JsonElement> DeserializeOutputJSON(string output)
+        {
+            var resultDic = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(output);
+            return resultDic;
+        }
+    }
+}

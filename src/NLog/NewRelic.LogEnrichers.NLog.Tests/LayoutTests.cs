d51d016e433781e25d09d3526449e6910fe19811
Alex Hemsath <ahemsath@newrelic.com>
2019-12-13 11:46:21 -0800

    Add test to verify linking metadata

--- src/NLog/NewRelic.LogEnrichers.NLog.Tests/LayoutTests.cs
+++ src/NLog/NewRelic.LogEnrichers.NLog.Tests/LayoutTests.cs
@@ -5,6 +5,8 @@ using NLog;
 using System.Threading;
 using System.Diagnostics;
 using NewRelic.Api.Agent;
+using Telerik.JustMock;
+using System.Collections.Generic;
 
 namespace NewRelic.LogEnrichers.NLog.Tests
 {
@@ -12,13 +14,25 @@ namespace NewRelic.LogEnrichers.NLog.Tests
     {
         Logger _logger;
         DebugTarget _target;
-        IAgent _agent;
+        IAgent _testAgent;
+
+        Dictionary<string,string> linkingMetadataDict = new Dictionary<string, string>
+            {
+                { "trace.id", "trace-id" },
+                { "span.id", "span-id" },
+                { "entity.name", "entity-name" },
+                { "entity.type", "entity-type" },
+                { "entity.guid", "entity-guid" },
+                { "hostname", "host-name" }
+            };
+
 
         [SetUp]
         public void Setup()
         {
+            _testAgent = Mock.Create<IAgent>();
             _target = new DebugTarget("testTarget");
-            _target.Layout = new NewRelicJsonLayout(() => _agent);
+            _target.Layout = new NewRelicJsonLayout(() => _testAgent);
 
             var config = new LoggingConfiguration();
             config.AddTarget(_target);
@@ -30,19 +44,52 @@ namespace NewRelic.LogEnrichers.NLog.Tests
         }
 
         [Test]
-        public void LogMessage_VerifyAttributes()
+        public void LogMessage_NoAgent_VerifyAttributes()
         {
+            //Arrange
             var message = "lorem ipsum";
-            _logger.Info(message);
 
+            //Act
+            _logger.Info(message);
             var loggedMessage = _target.LastMessage;
             var resultsDictionary = TestHelpers.DeserializeOutputJSON(loggedMessage);
 
+            //Assert
             Asserts.KeyAndValueMatch(resultsDictionary, "message", message);
             Asserts.KeyAndValueMatch(resultsDictionary, "log.level", "INFO");
             Asserts.KeyAndValueMatch(resultsDictionary, "thread.id", Thread.CurrentThread.ManagedThreadId.ToString());
             Asserts.KeyAndValueMatch(resultsDictionary, "process.id", Process.GetCurrentProcess().Id.ToString());
             Assert.IsTrue(resultsDictionary.ContainsKey("timestamp"));
+        }
+
+        [Test]
+        public void LogMessage_WtihAgent_VerifyAttributes()
+        {
+            //Arrange
+            var wasRun = false;
+            Mock.Arrange(() => _testAgent.GetLinkingMetadata())
+                .DoInstead(() => { wasRun = true; })
+                .Returns(linkingMetadataDict);
+
+            var message = "lorem ipsum";
+
+            //Act
+            _logger.Info(message);
+            var loggedMessage = _target.LastMessage;
+            var resultsDictionary = TestHelpers.DeserializeOutputJSON(loggedMessage);
+
+            //Assert
+            Asserts.KeyAndValueMatch(resultsDictionary, "message", message);
+            Asserts.KeyAndValueMatch(resultsDictionary, "log.level", "INFO");
+            Asserts.KeyAndValueMatch(resultsDictionary, "thread.id", Thread.CurrentThread.ManagedThreadId.ToString());
+            Asserts.KeyAndValueMatch(resultsDictionary, "process.id", Process.GetCurrentProcess().Id.ToString());
+            Assert.IsTrue(resultsDictionary.ContainsKey("timestamp"));
+            Assert.IsTrue(wasRun);
+            foreach (var key in linkingMetadataDict.Keys)
+            {
+                Asserts.KeyAndValueMatch(resultsDictionary, key, linkingMetadataDict[key]);
+            }
+
         }
     }
 }

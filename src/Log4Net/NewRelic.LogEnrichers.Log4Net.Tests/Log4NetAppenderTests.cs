efe126110593a554f7ec926c13193dd05b7c8b0a
Vu Tran <vuqtran88@gmail.com>
2020-01-23 15:34:24 -0800

    newrelic log4net appender

---
+++ src/Log4Net/NewRelic.LogEnrichers.Log4Net.Tests/Log4NetAppenderTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net.Core;
+using NewRelic.Api.Agent;
+using NUnit.Framework;
+using Telerik.JustMock;
+
+namespace NewRelic.LogEnrichers.Log4Net.Tests
+{
+	public class Log4NetAppenderTests
+    {
+        private IAgent _testAgent;
+
+        [SetUp]
+		public void Setup()
+		{
+            _testAgent = Mock.Create<IAgent>();
+        }
+
+        [Test]
+		public void GetLinkingMetadata_CalledOnceForEachEvent()
+		{
+            // Arrange
+            const string countLogMessage = "This is a log message";
+
+            var testAppender = new NewRelicAppender(() => _testAgent);
+            testAppender.ActivateOptions();
+
+            //Set the the NewRelicAppender at the root logger
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            BasicConfigurator.Configure(logRepository, testAppender);
+
+            var testLogger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            var rnd = new Random();
+            var countLogAttempts = rnd.Next(2, 25);
+
+            //// Act
+            for (var i = 0; i < countLogAttempts; i++)
+            {
+                testLogger.Info(countLogMessage);
+            }
+
+            // Assert
+            Mock.Assert(() => _testAgent.GetLinkingMetadata(), Occurs.Exactly(countLogAttempts));
+        }
+
+        [Test]
+        public void GetLinkingMetadata_IsHandled_NullAgent()
+        {
+            // Arrange
+            var testAppender = new NewRelicAppender(() => _testAgent);
+            testAppender.ActivateOptions();
+
+            //Set the the NewRelicAppender at the root logger
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            BasicConfigurator.Configure(logRepository, testAppender);
+
+            var testLogger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            var layout = Mock.Create<log4net.Layout.SimpleLayout>();
+            layout.ActivateOptions();
+
+            testAppender.Layout = layout;
+            var childAppender = new log4net.Appender.ConsoleAppender();
+            childAppender.Layout = layout;
+
+            testAppender.AddAppender(childAppender);
+
+            var testLoggingEvents = new List<LoggingEvent>();
+
+            Mock.Arrange(() => layout.Format(Arg.IsAny<TextWriter>(), Arg.IsAny<LoggingEvent>())).DoInstead((TextWriter textWriter, LoggingEvent loggingEvent) => 
+            {
+                testLoggingEvents.Add(loggingEvent);
+            });
+
+            // Act
+
+            var rnd = new Random();
+            var countLogAttempts = rnd.Next(2, 25);
+
+            for (var i = 0; i < countLogAttempts; i++)
+            {
+                testLogger.Info("This is log message " + i);
+            }
+
+            // Assert
+            testLoggingEvents.ForEach(loggingEvent =>
+            {
+                Assert.That(!loggingEvent.Properties.Contains("newrelic.linkingmetadata"), "newrelic.linkingmetadata property found. This test does not expect newrelic.linkingmetadata property in log event");
+            });
+        }
+
+    }
+}
---
+++ src/Log4Net/NewRelic.LogEnrichers.Log4Net/NewRelicAppender.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using log4net.Appender;
+using log4net.Core;
+using NewRelic.Api.Agent;
+
+namespace NewRelic.LogEnrichers.Log4Net
+{
+	public class NewRelicAppender : ForwardingAppender
+	{
+        private const string LinkingMetadataKey = "newrelic.linkingmetadata";
+        private readonly Lazy<IAgent> _nrAgent;
+
+        internal NewRelicAppender(Func<IAgent> agentFactory)
+        {
+            _nrAgent = new Lazy<IAgent>(agentFactory);
+        }
+
+        public NewRelicAppender() : this(NewRelic.Api.Agent.NewRelic.GetAgent)
+        {
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            var linkingMetadata = _nrAgent.Value?.GetLinkingMetadata();
+
+            if (linkingMetadata != null && linkingMetadata.Keys.Count != 0)
+            {
+                loggingEvent.Properties[LinkingMetadataKey] = linkingMetadata;
+            }
+
+            base.Append(loggingEvent);
+        }
+
+        protected override void Append(LoggingEvent[] loggingEvents)
+        {
+            var linkingMetadata = _nrAgent.Value?.GetLinkingMetadata();
+
+            if (linkingMetadata != null && linkingMetadata.Keys.Count != 0)
+            {
+                foreach (var loggingEvent in loggingEvents)
+                {
+                    loggingEvent.Properties[LinkingMetadataKey] = linkingMetadata;
+                }
+            }
+
+            base.Append(loggingEvents);
+        }
+    }
+}

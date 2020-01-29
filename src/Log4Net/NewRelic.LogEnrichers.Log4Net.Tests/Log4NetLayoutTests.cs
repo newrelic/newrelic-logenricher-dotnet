b45eb5467a6e8c78131ccab2ea31b821a9ac8be5
Vu Tran <vuqtran88@gmail.com>
2020-01-28 16:26:25 -0800

    NewRelicLayout

---
+++ src/Log4Net/NewRelic.LogEnrichers.Log4Net.Tests/Log4NetLayoutTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net.Core;
+using NewRelic.Api.Agent;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Telerik.JustMock;
+
+namespace NewRelic.LogEnrichers.Log4Net.Tests
+{
+    public class Log4NetLayoutTests
+    {
+
+        [TestCase("INFO")]
+        [TestCase("WARN")]
+        [TestCase("FATAL")]
+        [TestCase("ERROR")]
+        public void Output_Intrinsics_LogLevel(string level)
+        {
+            // Arrange
+            LogManager.ShutdownRepository(Assembly.GetEntryAssembly());
+
+            var testAgent = Mock.Create<IAgent>();
+            Mock.Arrange(() => testAgent.GetLinkingMetadata()).Returns(new Dictionary<string, string>() { { "key1", "value1" } });
+
+            var testAppender = new NewRelicAppender(() => testAgent);
+
+            //Set the the NewRelicAppender at the root logger
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            BasicConfigurator.Configure(logRepository, testAppender);
+
+            var layout = Mock.Create<NewRelicLayout>();
+
+            var childAppender = new log4net.Appender.ConsoleAppender
+            {
+                Layout = layout
+            };
+
+            testAppender.AddAppender(childAppender);
+
+            TextWriter tw = null;
+
+            Mock.Arrange(() => layout.Format(Arg.IsAny<TextWriter>(), Arg.IsAny<LoggingEvent>())).DoInstead((TextWriter textWriter, LoggingEvent loggingEvent) =>
+            {
+                tw = textWriter;
+            }).CallOriginal();
+
+            var testLogger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            //// Act
+            testLogger.Log(level, "This is a log message");
+
+            var serializedMessage = tw.ToString();
+            var deserializedMessage = TestHelpers.DeserializeOutputJSON(serializedMessage);
+
+            // Assert
+            Assert.That(deserializedMessage.ContainsKey("log.level"), "log.level not found.");
+            var resultLevel = TestHelpers.DeserializeOutputJSON(deserializedMessage["log.level"].ToString());
+            Assert.That(resultLevel["Name"].ToString() == level, "Incorrect logging level");
+        }
+    }
+
+    public static class CustomLoggingExtensions
+    {
+        public static void Log(this ILog logger, string level, object message)
+        {
+            switch (level.ToLower()) 
+            {
+                case "info":
+                    logger.Info(message);
+                    break;
+                case "debug":
+                    logger.Debug(message);
+                    break;
+                case "warn":
+                    logger.Warn(message);
+                    break;
+                case "fatal":
+                    logger.Fatal(message);
+                    break;
+                case "error":
+                    logger.Error(message);
+                    break;
+                default:
+                    throw new Exception(@$"level:{level} not recognized logging level.");
+            }
+        }
+    }
+}
+
---
+++ src/Log4Net/NewRelic.LogEnrichers.Log4Net/NewRelicLayout.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using log4net.Core;
+using log4net.Layout;
+using Newtonsoft.Json;
+
+namespace NewRelic.LogEnrichers.Log4Net
+{
+    public class NewRelicLayout : LayoutSkeleton
+    {
+        private const string LinkingMetadataKey = "newrelic.linkingmetadata";
+
+        public NewRelicLayout()
+        {
+            base.IgnoresException = false;
+        }
+        public override void ActivateOptions()
+        {
+        }
+
+        public override void Format(TextWriter writer, LoggingEvent e)
+        {
+            var dic = new Dictionary<string, object>();
+
+            SetInstrinsics(dic, e);
+            SetExceptionData(dic, e);
+            SetUserProperties(dic, e);
+            SetLinkMetaData(dic, e);
+
+            writer.WriteLine(JsonConvert.SerializeObject(dic));
+        }
+
+        void SetInstrinsics(Dictionary<string, object> dictionary, LoggingEvent e)
+        {
+            if (dictionary == null)
+            {
+                return;
+            }
+
+            dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.Timestamp), e.TimeStamp.ToUnixTimeMilliseconds());
+            dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.ThreadName), e.ThreadName);
+            dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.MessageText), e.RenderedMessage);
+            dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.LogLevel), e.Level);
+        }
+
+        void SetExceptionData(Dictionary<string, object> dictionary, LoggingEvent e) 
+        {
+            if (dictionary == null)
+            {
+                return;
+            }
+
+            if (e.ExceptionObject != null)
+            {
+                dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.ErrorMessage), e.ExceptionObject?.Message);
+                dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.ErrorStack), e.ExceptionObject?.StackTrace);
+                dictionary.Add(LoggingExtensions.GetOutputName(NewRelicLoggingProperty.ErrorClass), e.ExceptionObject?.GetType().ToString());
+            }
+        }
+
+        void SetUserProperties(Dictionary<string, object> dictionary, LoggingEvent e)
+        {
+            if (dictionary == null)
+            {
+                return;
+            }
+
+            foreach (var key in e.Properties.GetKeys())
+            {
+                if (key != LinkingMetadataKey)
+                {
+                    dictionary[LoggingExtensions.UserPropertyPrefix + key] = e.Properties[key];
+                }
+            }
+        }
+
+        void SetLinkMetaData(Dictionary<string, object> dictionary, LoggingEvent e)
+        {
+            if (dictionary == null)
+            {
+                return;
+            }
+
+            if (e.Properties[LinkingMetadataKey] is Dictionary<string, string> linkdata)
+            {
+                foreach (var kv in linkdata)
+                {
+                    dictionary[kv.Key] = kv.Value;
+                }
+            }
+        }
+
+    }
+
+}

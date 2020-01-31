f6d92f91fe8cb720a7199af50fd92002b858794c
Vu Tran <vuqtran88@gmail.com>
2020-01-31 13:39:15 -0800

    Sample app uses NewRelic.LogEnrichers.Log4Net to output NewRelic json format log

---
+++ examples/NewRelic.LogEnrichers.Log4Net.Examples/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using log4net;
+using log4net.Config;
+using NewRelic.Api.Agent;
+
+
+namespace NewRelic.LogEnrichers.Log4Net.Examples
+{
+    static class Program
+    {
+        private static ILog _logger;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the New Relic Logging Extentions for Log4Net");
+            Console.WriteLine();
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("dotnet run {OutputFolderPath}");
+                return;
+            }
+
+            var folderPath = string.Join(' ', args);
+
+            var folderPath_StandardLogs = Path.Combine(folderPath, "StandardLogs");
+            var folderPath_NewRelicLogs = Path.Combine(folderPath, "NewRelicLogs");
+
+            // Ensure that the output folders exist
+            Directory.CreateDirectory(folderPath_StandardLogs);
+            Directory.CreateDirectory(folderPath_NewRelicLogs);
+
+            GlobalContext.Properties["StandardLogFileName"] = @$"{folderPath_StandardLogs}\Log4NetExample.log";
+            GlobalContext.Properties["NewRelicLogFileName"] = @$"{folderPath_NewRelicLogs}\Log4NetExample.json";
+
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            // This log information will be visible in New Relic Logging. Since 
+            // a transaction has not been started, this log message will not be
+            // associated to a specific transaction.
+            _logger.Info("Hello, welcome to the Log4Net Logs In Context sample app!");
+
+            do
+            {
+                Console.WriteLine("Creating Logged Transactions");
+
+                // Call example methods that create transactions
+                TestMethod();
+                TestExceptionMethod();
+
+                Console.WriteLine("Press <ENTER> to continue, Q to exit.");
+            }
+            while (Console.ReadLine() != "Q");
+
+            // This log information will be visible in New Relic Logging. Since 
+            // a transaction has not been started, this log message will not be
+            // associated to a specific transaction.
+            _logger.Info("Thanks for visiting, please come back soon!");
+        }
+
+        [Transaction]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void TestMethod()
+        {
+            _logger.Info(@$"Starting TestMethod");
+
+            try
+            {
+                for (var cnt = 0; cnt < 10; cnt++)
+                {
+                    Console.WriteLine("writing message");
+                    _logger.Info(@$"This is log message {cnt}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(@$"Error has occurred in TestMethod", ex);
+            }
+
+            _logger.Info(@$"Ending TestMethod");
+        }
+
+        [Transaction]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void TestExceptionMethod()
+        {
+            _logger.Info(@$"Starting TestExceptionMethod");
+
+            for (var cnt = 0; cnt < 10; cnt++)
+            {
+                try
+                {
+                    throw new Exception("An exception has occurred");
+                }
+                catch (Exception ex)
+                {
+                    NewRelic.Api.Agent.NewRelic.NoticeError(ex);
+                    _logger.Error(@$"Error has occurred in TestExceptionMethod - exception number {cnt}.", ex);
+                }
+            }
+
+            _logger.Info(@$"Ending TestExceptionMethod");
+        }
+    }
+}

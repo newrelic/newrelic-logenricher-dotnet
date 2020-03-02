bcbec7b53cc523de35ec6eaf178c61b646e1425f
Alex Hemsath <ahemsath@newrelic.com>
2020-03-02 11:17:49 -0800

    Fix user properties key for NLog

--- src/NLog/NewRelic.LogEnrichers.NLog.Tests/LayoutTests.cs
+++ src/NLog/NewRelic.LogEnrichers.NLog.Tests/LayoutTests.cs
@@ -23,7 +23,7 @@ namespace NewRelic.LogEnrichers.NLog.Tests
 
         private const string TestErrMsg = "This is a test exception";
         private const string LogMessage = "This is a log message";
-        private const string UserPropertiesKey = "Message Properties";
+        private readonly string UserPropertiesKey = NewRelicJsonLayout.UserPropertyKey;
 
         private static readonly Dictionary<string,string> linkingMetadataDict = new Dictionary<string, string>
             {

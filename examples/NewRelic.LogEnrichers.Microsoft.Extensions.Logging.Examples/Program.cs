a5598eff6f00a1584d3a29507542b0f855b21f93
lspangler584 <20267975+lspangler584@users.noreply.github.com>
2021-02-25 17:19:41 -0800

    add skeleton MEL projects

---
+++ examples/NewRelic.LogEnrichers.Microsoft.Extensions.Logging.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewRelic.LogEnrichers.Microsoft.Extensions.Logging.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/NewRelic.LogEnrichers.Microsoft.Extensions.Logging/LogRecord.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace NewRelic.LogEnrichers.Microsoft.Extensions.Logging
+{
+    public sealed class LogRecord
+    {
+        internal LogRecord(DateTime timestamp, string categoryName, LogLevel logLevel, EventId eventId, object state, Exception exception)
+        {
+            Timestamp = timestamp;
+            CategoryName = categoryName;
+            LogLevel = logLevel;
+            EventId = eventId;
+            State = state;
+            Exception = exception;
+        }
+
+        public DateTime Timestamp { get; }
+
+        public string CategoryName { get; }
+
+        public LogLevel LogLevel { get; }
+
+        public EventId EventId { get; }
+
+        public object State { get; }
+
+        public Exception Exception { get; }
+    }
+}
